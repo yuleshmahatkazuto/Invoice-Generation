481f45fb954bf078e0138d3ec248b45ff6a84d00
Yulesh Mahat <yuleshkazuto@gmail.com>
2025-02-04 20:06:52 +1100

    work on UI/UX part of the home page.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express, { response } from "express";
 import bodyParser from "body-parser";
 import path from "path";
-import { fileURLToPath } from 'url';
-import axios from 'axios';
-import {timeExtractor, payCalculator, timeDiffCalculator, timeConverter} from "./payCalculator.js";
+import { fileURLToPath } from "url";
+import axios from "axios";
+import {
+  timeExtractor,
+  payCalculator,
+  timeDiffCalculator,
+  timeConverter,
+} from "./payCalculator.js";
 
 const app = express();
 const port = 3000;
@@ -11,13 +16,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const clientId = "753345";
-const clientSecret = "c5c685a22e55484bafc32256f124d11b"
+const clientSecret = "c5c685a22e55484bafc32256f124d11b";
 const authURL = "https://go.servicem8.com/oauth/authorize";
-const redirectUri = "https://eb8b-122-105-230-232.ngrok-free.app/callback";
-const my_UUID = '1516b609-0860-4921-a15a-2027953c8f3b';
+const redirectUri = "https://0c93-122-105-228-74.ngrok-free.app/callback";
+const my_UUID = "1516b609-0860-4921-a15a-2027953c8f3b";
 let access_token, expires_in, refresh_token;
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -33,39 +38,36 @@ app.get("/auth", (req, res) => {
   res.redirect(authURL + params);
 });
 
-app.get("/callback", async(req, res) => {
-
-  if(access_token){
-    return res.sendFile(path.join(__dirname, "public", "home.html" ));
-  }
-
-  const authorizationCode = req.query.code;
-
-  if(!authorizationCode){
-    return res.status(400).send("Authorization code not provided by Servicem8");
-  }
-
-  try{
-    const response = await axios.post(`https://api.servicem8.com/oauth/access_token`,{
-      client_id: clientId,
-      client_secret: clientSecret,
-      code: authorizationCode,
-      redirect_uri: redirectUri,
-      grant_type: "authorization_code"
-    });
-
-    ({access_token, expires_in, refresh_token} = response.data);
-    console.log("Full response.data" + JSON.stringify(response.data));
-    res.sendFile(path.join(__dirname, "public", "home.html"));
-  }
-  catch(error){
-    res.send("An error occured!");
-  }
-
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
-let startDate = "";
-let endDate = "";
+app.get("/callback", async (req, res) => {
+  const authorizationCode = req.query.code;
+  if (!authorizationCode) {
+    return res.status(400).send("Authorization code not provided by Servicem8");
+  }
+  try {
+    const response = await axios.post(
+      `https://api.servicem8.com/oauth/access_token`,
+      {
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: authorizationCode,
+        redirect_uri: redirectUri,
+        grant_type: "authorization_code",
+      }
+    );
+    ({access_token, refresh_token, expires_in} = response.data);
+    console.log(`Your access token is ${access_token}`);
+    res.redirect("/home");
+  } catch (error) {
+    res.send("An error occured!");
+  }
+});
+
+let startDate, endDate, invoiceNo = "";
+
 app.get("/submission_form", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "submissionForm.html"));
 });
@@ -73,83 +75,104 @@ app.get("/submission_form", (req, res) => {
 let arrayOfObjects = [];
 app.post("/submit", (req, res) => {
   const inputData = req.body.notes;
-    const eachDay = inputData.replace(/\r/g,"").split("\n").filter(line => line.trim() !== "");
-    
-    arrayOfObjects = timeExtractor(eachDay);
-    res.redirect('/callback');
+  const eachDay = inputData
+    .replace(/\r/g, "")
+    .split("\n")
+    .filter((line) => line.trim() !== "");
+
+  arrayOfObjects = timeExtractor(eachDay);
+  res.redirect("/home");
 });
 
 app.post("/getDate", (req, res) => {
   startDate = req.body.startdate;
   endDate = req.body.enddate;
-  res.redirect("/callback");
-})
+  invoiceNo = req.body.invoiceNo;
+  res.redirect("/home");
+});
 
-app.get("/jobs", async(req, res) => {
+app.get("/jobs", async (req, res) => {
   let payMap = timeFormatConverter(arrayOfObjects);
-  console.log(payMap);
-  if(!access_token){
+  if (!access_token) {
     res.send("Access token not available yet");
-  }else{
-    try{
-      const jobresponse = await axios.get("https://api.servicem8.com/api_1.0/Job.json", {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-        params: {
-          $filter: `completion_date gt '${startDate} 00:00:00'`,
-        } 
-      });
-      const companyResponse = await axios.get("https://api.servicem8.com/api_1.0/JobContact.json",{
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-      });
+  } else {
+    try {
+      const jobresponse = await axios.get(
+        "https://api.servicem8.com/api_1.0/Job.json",
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+          params: {
+            $filter: `completion_date gt '${startDate} 00:00:00'`,
+          },
+        }
+      );
+      const companyResponse = await axios.get(
+        "https://api.servicem8.com/api_1.0/JobContact.json",
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
 
       const customerName = new Map();
-      companyResponse.data.forEach(customer => {
-        customerName.set(customer.job_uuid, customer.first + " " + customer.last);
+      companyResponse.data.forEach((customer) => {
+        customerName.set(
+          customer.job_uuid,
+          customer.first + " " + customer.last
+        );
       });
 
-      const jobs = jobresponse.data.filter(job => job.completion_date.split(" ")[0] !== endDate);
+      const jobs = jobresponse.data.filter(
+        (job) => job.completion_date.split(" ")[0] <= endDate
+      );
       const jobsById = filterJobsByCompletionId(jobs);
       const jobsByDate = [];
       const dateCount = {};
-      jobsById.forEach(job => {
-        const date = job.completion_date.split(" ")[0];  // Extract the date part
-        dateCount[date] = (dateCount[date] || 0) + 1;  // Increment count for this date
+      jobsById.forEach((job) => {
+        const date = job.completion_date.split(" ")[0]; // Extract the date part
+        dateCount[date] = (dateCount[date] || 0) + 1; // Increment count for this date
       });
-      jobsById.forEach(job => {
+      jobsById.forEach((job) => {
         const date = job.completion_date.split(" ")[0];
-        
+
         const customer_Name = customerName.get(job.uuid) || "";
-        let dateEntry = jobsByDate.find(entry => entry.date === date);
-        if(!dateEntry){
-          dateEntry = {date: date, jobs: []};
+        let dateEntry = jobsByDate.find((entry) => entry.date === date);
+        if (!dateEntry) {
+          dateEntry = { date: date, jobs: [] };
           jobsByDate.push(dateEntry);
         }
         dateEntry.jobs.push({
           rowSpan: dateCount[date],
           jobID: job.generated_job_id,
           date: date,
-          customerName : customer_Name,
+          customerName: customer_Name,
           jobAddress: job.geo_city,
           labourCharge: payMap.get(date),
           paymentMethod: job.payment_method,
         });
       });
-      console.log(dateCount);
-      res.render(path.join(__dirname, "views/invoice.ejs"), {jobs: jobsByDate});
-    }catch(error){
-      console.error("Error fetching job details:", error.response?.data || error.message);
+      res.render(path.join(__dirname, "views/invoice.ejs"), {
+        jobs: jobsByDate,
+        invoiceNo: invoiceNo,
+      });
+    } catch (error) {
+      console.error(
+        "Error fetching job details:",
+        error.response?.data || error.message
+      );
     }
   }
 });
 
- //helper function to filter the response.data using my uuid.
- function filterJobsByCompletionId(jobs){
-  const jobsById = jobs.filter(job => {
-    if(!job.completion_date){return false;}
+//helper function to filter the response.data using my uuid.
+function filterJobsByCompletionId(jobs) {
+  const jobsById = jobs.filter((job) => {
+    if (!job.completion_date) {
+      return false;
+    }
     const jobDate = job.completion_date.split(" ")[0];
     return job.completion_actioned_by_uuid === my_UUID;
   });
@@ -157,12 +180,12 @@ app.get("/jobs", async(req, res) => {
 }
 
 //to convert the original time from the notes to the format of servicem8
-function timeFormatConverter(array){
+function timeFormatConverter(array) {
   const datePayMap = new Map();
-  array.forEach(day => {
+  array.forEach((day) => {
     let dateArr = day.original.split(" ");
     let month;
-    switch (dateArr[2]){
+    switch (dateArr[2]) {
       case "Jan":
         month = 1;
         break;
@@ -172,7 +195,7 @@ function timeFormatConverter(array){
       case "Mar":
         month = 3;
         break;
-        case "Apr":
+      case "Apr":
         month = 4;
         break;
       case "May":
@@ -181,7 +204,7 @@ function timeFormatConverter(array){
       case "Jun":
         month = 6;
         break;
-        case "Jul":
+      case "Jul":
         month = 7;
         break;
       case "Aug":
@@ -190,7 +213,7 @@ function timeFormatConverter(array){
       case "Sep":
         month = 9;
         break;
-        case "Oct":
+      case "Oct":
         month = 10;
         break;
       case "Nov":
