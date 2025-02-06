b4a81de2e84c74054ffb04c6b5c7b247197e3112
Yulesh Mahat <yuleshkazuto@gmail.com>
2025-02-06 21:48:56 +1100

    yet to fix the barrier plus normal day pay calculation.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 const clientId = "753345";
 const clientSecret = "c5c685a22e55484bafc32256f124d11b";
 const authURL = "https://go.servicem8.com/oauth/authorize";
-const redirectUri = "https://08c1-122-105-228-174.ngrok-free.app/callback";
+const redirectUri = "https://b2c1-122-105-231-96.ngrok-free.app/callback";
 const my_UUID = "1516b609-0860-4921-a15a-2027953c8f3b";
 let access_token, expires_in, refresh_token;
 
@@ -182,6 +182,11 @@ function filterJobsByCompletionId(jobs) {
 //to convert the original time from the notes to the format of servicem8
 function timeFormatConverter(array) {
   const datePayMap = new Map();
+  const monthMap = {
+    Jan: "01", Feb: "02", Mar: "03", Apr: "04", May: "05", Jun: "06",
+    Jul: "07", Aug: "08", Sep: "09", Oct: "10", Nov: "11", Dec: "12"
+  };
+  
   array.forEach((day) => {
     let dateArr = day.original.split(" ");
     let month;
@@ -223,9 +228,9 @@ function timeFormatConverter(array) {
         month = 12;
         break;
     }
-    let date = "2025-" + month.toString().padStart(2, "0") + "-" + dateArr[1];
+    let date = "2025-" + month.toString().padStart(2, "0") + "-" + dateArr[1].padStart(2,"0");
     if(datePayMap.has(date)){
-      datePayMap.set(date, datePayMap.get(date) + day.pay)
+      datePayMap.set(date, (parseFloat(datePayMap.get(date)) + parseFloat(day.pay)));
     }else{
       datePayMap.set(date, day.pay);
     }    
